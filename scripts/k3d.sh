066eb1b21f79edaa6156b69495fe4c5fae17eac3
Raja Soundaramourty <rajasoun@icloud.com>
2022-11-06 09:24:57 +0530

    refactor apps

--- scripts/k3d.sh
+++ scripts/k3d.sh
@@ -3,14 +3,16 @@
 SCRIPT_LIB_DIR="$(dirname "${BASH_SOURCE[0]}")"
 
 export CLUSTER_NAME=${CLUSTER_NAME:-"spike"}
+export $(grep -v '^#' config/.env | xargs) 
 
 function setup(){
     # try k3d cluster create $CLUSTER_NAME --k3s-arg="--disable=traefik@server:*"
-    cat config/k3d-config.yaml | envsubst > /tmp/k3d-config.yaml
-    try k3d cluster create --config  /tmp/k3d-config.yaml
+    # cat config/k3d-config.yaml | envsubst > /tmp/k3d-config.yaml
+
+    # Load .env 
+    try k3d cluster create --registry-config <(cat config/registries.yaml | envsubst) --config  <(cat config/k3d-config.yaml | envsubst)
     export KUBECONFIG=$(k3d kubeconfig write $CLUSTER_NAME)
-    echo "export KUBECONFIG=${KUBECONFIG}"
-    rm /tmp/k3d-config.yaml
+    echo "KUBECONFIG=${KUBECONFIG}"
 
     add_host_entry "127.0.0.1" "k3d.local"
     
